f5af2e2419f59ac82ad4c90b29edd9c19ad3bbee
MohamedShaker <mshaker809@yahoo.com>
2022-03-05 18:42:05 +0200

    adding sleuth to gateway server

--- zuul-apigateway-server/src/main/java/com/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ zuul-apigateway-server/src/main/java/com/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -1,9 +1,11 @@
 package com.microservices.springcloudconfigserver;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -15,4 +17,8 @@ public class SpringCloudConfigServerApplication {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
 
+	@Bean
+	public Sampler defaultSample(){
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
