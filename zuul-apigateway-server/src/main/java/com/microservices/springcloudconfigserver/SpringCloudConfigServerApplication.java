97d45d7acf28fba01b5a9db0a264d7bb6049c56b
MohamedShaker <mshaker809@yahoo.com>
2022-03-05 18:29:21 +0200

    intialize zuul api gateway server

---
+++ zuul-apigateway-server/src/main/java/com/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
---
+++ zuul-apigateway-server/src/main/java/com/microservices/springcloudconfigserver/logging/ZuulLoggingFilter.java
@@ -0,0 +1,33 @@
+package com.microservices.springcloudconfigserver.logging;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+        System.out.println("request:"+request);
+        return null;
+    }
+}
